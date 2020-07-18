273a3d297f434f7abc39de2bcb878d9b9cb02f93
bdavis90 <wowzerss90@yahoo.com>
2020-07-17 21:34:53 -0400

    finished ex4

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,16 +2,18 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
+    hash_table = {}
+    
     # Your code here
 
     return result
 
 
-if __name__ == "__main__":
-    arrays = []
+# if __name__ == "__main__":
+#     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+#     arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+#     arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+#     arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
 
-    print(intersection(arrays))
+#     print(intersection(arrays))
