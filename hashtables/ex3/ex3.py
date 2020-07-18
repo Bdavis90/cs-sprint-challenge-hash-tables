99a8f071b7ceab29cf31e5bda7ad71a9d225a7c2
bdavis90 <wowzerss90@yahoo.com>
2020-07-17 21:51:04 -0400

    finished ex3

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,10 +3,26 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     hash_table = {}
-    
-    # Your code here
+    results = []
 
-    return result
+    for array in arrays:
+        for num in array:
+            if num not in hash_table:
+                hash_table[num] = 1
+            else:
+                hash_table[num] += 1
+
+    pairs = hash_table.items()
+    
+    for key, value in pairs:
+       if value == len(arrays):
+           results.append(key)
+
+    # print([key for key, value in hash_table.items() if value == len(arrays)])
+    # return only the items that have an occurence equal to the length of the arrays
+    return results
+
+print(intersection([[1,2,4,5,6,7], [1,4,7,6],[1,9,0,7]]))
 
 
 # if __name__ == "__main__":
